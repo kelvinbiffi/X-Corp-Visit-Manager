ffdf1b05fb16a7a1f73bdb8eedbfc06285b5be4c
kelvinbiffi <kelvin.biffi@hotmail.com>
2017-04-15 18:34:46 -0300

    Started develop verify schedule visits to get same salesperson by week and city

--- controller/controllerVisitas.js
+++ controller/controllerVisitas.js
@@ -22,11 +22,11 @@ module.exports = function() {
    * Reinicializar campos de retorno
    */
   var clearRetorno = function(post){
-    retorno.city = (post.city ? post.city : "");
-    retorno.date = (post.date ? post.date : "");
     retorno.name = (post.name ? post.name : "")
     retorno.email = (post.email ? post.email : "")
     retorno.fone = (post.fone ? post.fone : "")
+    retorno.city = (post.city ? post.city : "");
+    retorno.date = (post.date ? post.date : "");
     retorno.erro = "";
   };
 
@@ -34,7 +34,7 @@ module.exports = function() {
    * Callback ao salvar nova visita
    */
   var callbackSaveVisit = function(a){
-    console.log(a);
+    console.log(a, "retorno");
   };
 
   /**
@@ -43,18 +43,44 @@ module.exports = function() {
   var persistirNovaVisita = function(){
 
     var fs = require("fs");
+
     var jsonVisitas = require("../db/visitas.json");
-    console.log(jsonVisitas, "jsonVisitas");
-    // var vendedor = -1;
-    // if(jsonVisitas.visitas.length > 0){
-    //   //TODO verificar vendedores na semana na cidade selecionada, caso não tenha cendedores continuar lógica
-    // }
-    // if(vendedor == -1){
-    //   //TODO randomizar vendedor para a visita
-    // }
-    // //TODO salvar dados da visita
-    //
-    // fs.writeFile( "../db/visitas.json", JSON.stringify( jsonVisitas ), "utf8", salvarVisita );
+    var jsonVendedores = require("../db/vendedores.json");
+
+    var objNovaVisita;
+
+    if(jsonVisitas.visitas.length > 0){
+      //TODO verificar vendedores na semana na cidade selecionada, caso não tenha cendedores continuar lógica
+      for (var i = 0; i < jsonVisitas.visitas.length; i++) {
+        var visita = jsonVisitas.visitas[i];
+        if (visita.cidade == retorno.city) {
+          console.log("É A MESMA CIDADE");
+          var dataEscolhida = new Date(visita.date + " 00:00");
+          console.log(dataEscolhida.getDay()," DAY");
+          /*
+          * TODO Verificar pela função Date o dia da semana específico da visita salvar.
+          * Em seguida descobrir qual dia do més é a segunda e a sexta desta semana
+          * Por final verificar se a data que o cliente escolheu sta entre estas duas datas, caso sim pegar id do mesmo vendedores
+          * CASO NÃO proseguir
+          */
+        }
+      }
+    }else{
+      var idVendedor = Math.floor((Math.random() * jsonVendedores.vendedores.length));
+
+      objNovaVisita = {
+        vendedor: idVendedor,
+        nome: retorno.name,
+        email: retorno.email,
+        telefone: retorno.fone,
+        cidade: retorno.city,
+        date: retorno.date
+      };
+
+      jsonVisitas.visitas.push(objNovaVisita);
+    }
+
+    fs.writeFile( "./db/visitas.json", JSON.stringify( jsonVisitas ), "utf8", callbackSaveVisit );
 
   }
 
